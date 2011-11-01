cccd42e88945c4b2ad502e3c31221e175e0eecc4
Jens Bissinger <mail@jens-bissinger.de>
2011-11-01 23:18:36 +0100

    formatted specs

--- spec/lib/dbus_interface/class_spec.rb
+++ spec/lib/dbus_interface/class_spec.rb
@@ -5,10 +5,8 @@ describe "DBusInterface::Class" do
   it 'should map_dbus :default_iface' do
     module AWorkingDummyInterface
       extend ::DBusInterface::Class
-      map_dbus :default_iface => 'foo.bar',
-               :object_path => 'foo/bar'
+      map_dbus :default_iface => 'foo.bar', :object_path => 'foo/bar'
     end
-    
     AWorkingDummyInterface.default_iface.should == 'foo.bar'
     AWorkingDummyInterface.object_path.should == 'foo/bar'
   end

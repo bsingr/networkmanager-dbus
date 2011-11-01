455596c4551ec1607825297e93661bd6a421c74e
Jens Bissinger <mail@jens-bissinger.de>
2011-11-01 23:38:10 +0100

    fixed connectivity example

--- examples/connectivity.rb
+++ examples/connectivity.rb
@@ -3,5 +3,5 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '../lib/network_mana
 puts "Internet Connection: #{NetworkManager.internet_connection? ? 'yes' : 'no'}"
 
 puts "States are described as NM_STATE_... in NetworkManager::DBus::Root"
-puts "State is == #{NetworkManager::DBus::Root.instance.object.state}"
+puts "State is == #{NetworkManager::DBus::Root.instance.call('state'}"
  

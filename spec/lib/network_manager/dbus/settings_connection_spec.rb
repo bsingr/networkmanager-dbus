7fe9f38bf072ade2c89b160048fbcf5eace10175
Jens Bissinger <mail@jens-bissinger.de>
2011-11-13 21:11:13 +0100

    refactored connection spec

--- spec/lib/network_manager/dbus/settings_connection_spec.rb
+++ spec/lib/network_manager/dbus/settings_connection_spec.rb
@@ -26,25 +26,29 @@ describe "NetworkManager::DBus::SettingsConnection" do
     end.should raise_error(NetworkManager::DBus::SettingsConnection::SecretsCacheInvalidError)
   end
   
-  it 'should do update' do
-    network_manager_dbus_mock
-    con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
-    hash = con.settings.dup
-    hash['connection']['id'] = 'bar'
-    con.update hash
-    con.settings['connection']['id'].should == 'bar'
-  end
+  describe 'update' do
+    it 'should do update' do
+      network_manager_dbus_mock
+      con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
+      hash = con.settings.dup
+      hash['connection']['id'] = 'bar'
+      con.update hash
+      con.settings['connection']['id'].should == 'bar'
+    end
   
-  it 'should get #name' do
-    network_manager_dbus_mock
-    con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
-    con.name.should == 'Wired connection 1'
-  end
+    it 'should get #name' do
+      network_manager_dbus_mock
+      con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
+      con.name.should == 'Wired connection 1'
+    end
   
-  it 'should set #name=' do
-    network_manager_dbus_mock
-    con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
-    con.name = 'foo'
-    con.name.should == 'foo'
+    it 'should set #name=' do
+      network_manager_dbus_mock
+      con = NetworkManager::DBus::SettingsConnection.new @object_paths.first
+      con.name = 'foo'
+      con.name.should == 'foo'
+    end
+    
+    
   end
 end

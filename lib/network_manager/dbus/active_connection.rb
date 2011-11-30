71b6542693897b859ae211754d0b4aa094d13565
Jens Bissinger <mail@jens-bissinger.de>
2011-11-30 21:52:24 +0100

    added NetworkManager::DBus::ActiveConnection#connection

--- lib/network_manager/dbus/active_connection.rb
+++ lib/network_manager/dbus/active_connection.rb
@@ -1,4 +1,8 @@
 class NetworkManager::DBus::ActiveConnection
   include DBusInterface::Object
   map_dbus :default_iface => 'org.freedesktop.NetworkManager.Connection.Active'
+  
+  def connection
+    @connection ||= NetworkManager::DBus::SettingsConnection.new self['Connection']
+  end
 end
